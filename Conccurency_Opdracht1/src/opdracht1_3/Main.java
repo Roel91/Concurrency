26d72193d3e5c156c248cd5da17046865875c5d1
Roelevers <revers91@gmail.com>
2013-11-30 10:08:22 +0100

    Opdracht 1.3 werkt

--- Conccurency_Opdracht1/src/opdracht1_3/Main.java
+++ Conccurency_Opdracht1/src/opdracht1_3/Main.java
@@ -1,86 +1,38 @@
 package opdracht1_3;
 
-import java.util.ArrayList;
-
 public class Main {
 	public static void main(String[] args) {
 
 		Main op = new Main();
-		op.execute(16, 4);
-		// op.execute(50000);
-		// op.execute(100000);
-		// op.execute(200000);
-		// op.execute(400000);
-		// op.execute(800000);
+		op.execute(100);
+		op.execute(50000);
+		op.execute(100000);
+		op.execute(200000);
+		op.execute(400000);
+		op.execute(800000);
 	}
 
-	private void execute(int nGetallen, int nThreads) {
+	private void execute(int nGetallen) {
 		System.out.println("Meetresultaten voor een lijst met de grootte: " + nGetallen);
 		
-		// random list maken
-		RandomListGenerator rg = new RandomListGenerator();
-		int[] integers = rg.randomList(nGetallen);
-
+		long total = 0;
+		
 		for(int x = 0; x < 10; x++){
-			
-		ArrayList<Thread> threads = new ArrayList<>();
-		ArrayList<int[]> parts = new ArrayList<>();
-		int start = 0;
-
-		for (int t = 0; t < nThreads; t++) {
-			int [] part = new int[nGetallen / nThreads];
-			parts.add(part);
-
-			System.arraycopy(integers, start, part, 0, part.length);
-			start += part.length;
-
-			Thread thread = new Thread(new MyRunnable(part));
-			
-			threads.add(thread);
-		}
-
 			long startTime = System.currentTimeMillis();
-
-			for (Thread thread : threads) {
-				thread.start();
-			}
-		
-			try {
+			RandomListGenerator rg = new RandomListGenerator();
+			int[] integers = rg.randomList(nGetallen);
 			
-				for (Thread thread : threads) {
-					thread.join();
-				}
+			ThreadTree tree = new ThreadTree();
+			tree.build(integers);
 			
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-
-			int[] mergeint = merge(parts);
-
-			for (int i = 0; i < mergeint.length; i++) {
-				System.out.print("[" + mergeint[i] + "], ");
-			}
 			long endTime = System.currentTimeMillis();
-			System.out.println((endTime - startTime) + " ms,");
-			System.out.println("-------------------------------------------------------------");
-		}
-	}
-	
-	public static int[] merge(ArrayList<int[]> parts) {
-
-		int totalLenght = 0;
-		for(int[] part: parts) {
-			totalLenght += part.length;
+			long duration = endTime - startTime;
+			total = total + duration;
+			System.out.print(duration + " ms, ");
 			
-			for(int i : part) {
-				System.out.print(i + ", ");
-			}
-			System.out.println();
 		}
-		
-	    int[] answer = new int[totalLenght]; 
-	    
-	    
-	    return answer;
+		System.out.println();
+		System.out.println("Gemiddeld: " + (total/10) + " ms");
+		System.out.println("-------------------------------------------------------------");
 	}
 }
---
+++ Conccurency_Opdracht1/src/opdracht1_3/ThreadTree.java
@@ -0,0 +1,65 @@
+package opdracht1_3;
+
+public class ThreadTree {
+
+	private ThreadTree left, right;
+	private MyRunnable runnable;
+	private Thread thread;
+	
+	public int[] build(int[] integers) {
+		if(integers.length > 10000) {
+			
+			int splitPoint = (integers.length / 2);
+			
+			int[] part1 = new int[splitPoint];
+			int[] part2 = new int[splitPoint];
+
+			System.arraycopy(integers, 0, part1, 0, part1.length);
+			System.arraycopy(integers, part1.length, part2, 0, part2.length);
+			
+			left = new ThreadTree();
+			part1 = left.build(part1);
+			
+			right = new ThreadTree();
+			part2 =right.build(part2);
+			
+			integers = merge(part1, part2);
+		}
+		System.arraycopy(integers, 0, integers, 0, integers.length);
+		
+		runnable = new MyRunnable(integers);
+		thread = new Thread(runnable);		
+		thread.start();
+		try {
+			thread.join();		
+		}
+		catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		return integers;		
+	}
+
+	private int[] merge(int[] part1, int[] part2) {
+	    int[] answer = new int[part1.length + part2.length];
+	    int a = 0;
+	    int b = 0;
+	    int c = 0;
+
+	    while (a < part1.length && b < part2.length) {
+	        if (part1[a] < part2[b]) {      
+	            answer[c++] = part1[a++];
+	        } else {
+	        	answer[c++] = part2[b++];  
+	        }              
+	    }
+
+	    while (a < part1.length) { 
+	        answer[c++] = part1[a++];
+	    }    
+
+	    while (b < part2.length) {   
+	        answer[c++] = part2[b++];
+	    }    
+	    return answer;
+	}
+}
